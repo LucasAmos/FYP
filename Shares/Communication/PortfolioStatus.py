ada1bc89b3ac86f0e67e89bdd483c1aa1b869c62
lucasamos <amosdj@aston.ac.uk>
2016-02-21 20:15:15 +0000

    email logic

--- Shares/Communication/PortfolioStatus.py
+++ Shares/Communication/PortfolioStatus.py
@@ -3,22 +3,14 @@ from twilioSMS import SMS
 from Shares.share_data import share_data
 from manage import db
 from Shares.models import User
-from flask import render_template
 from PortfolioData import PortfolioData
 import datetime
 
 
 class PortfolioStatus():
-    #email.sendEmail("xkv1922c@gmail.com", "alerts@lucasamos.net", "Your portfolio status", html)
-
     email = Email("lucas_amos", "beadle10")
     sms = SMS("ACb3d2405e15df8441919994ce553eae4b", "41e85a6638606f578860825b750462c1")
-
-
     session = db.session()
-
-    #values = share_data.getportfoliovalues(user)
-
     today = datetime.datetime.today().weekday()
 
     print today

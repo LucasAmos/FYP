96e0cc8b6d6fcd8d612c2b9143f918e073a529be
lucasamos <amosdj@aston.ac.uk>
2016-02-29 13:48:08 +0000

    news

--- Shares/__init__.py
+++ Shares/__init__.py
@@ -14,7 +14,6 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '5\x91"\xee:/C\xfc\x03\x9c\x04C9\xdc\xae\x00]\xc8\xa4\x16~\xe5\xe16'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
 app.config['DEBUG'] = True
-app.config['THREADED'] = True
 db = SQLAlchemy(app)
 
 #configure authentication
